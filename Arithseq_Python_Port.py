b03bfa2e36b10fc0abc98cdda80d1bcfa8ffddd4
Coder-2917 <argyris.fellouris24@gmail.com>
2025-03-27 21:20:56 -0500

    Update Arithseq_Python_Port.py

--- Arithseq_Python_Port.py
+++ Arithseq_Python_Port.py
@@ -1,5 +1,8 @@
 print("\nArithmetic Sequence Calculator")
-print("a = The Nth Term of the Sequence")
+a = 
 print("b = The First Term of the Sequence")
 print("d = The distance between each term of the Sequence")
-print("n = ")
+print("n = The Nth Term of the Sequence\n")
+def arithmetic_sequence(b, d, n):
+  return (d * (n - 1) + b)
+
