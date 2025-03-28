9be64a1bba5a6c797ec51bf2c01bbedc670ba0fb
Coder-2917 <argyris.fellouris24@gmail.com>
2025-03-27 21:25:32 -0500

    Update Arithseq_Python_Port.py

--- Arithseq_Python_Port.py
+++ Arithseq_Python_Port.py
@@ -6,3 +6,11 @@ print("n = The Nth Term of the Sequence\n")
 def arithmetic_sequence(b, d, n):
   return (d * (n - 1) + b)
 
+b = int(input("b = "))
+d = int(input("d = "))
+n = int(input("n = "))
+
+print(f"An Arithmetic Sequence with a first term of{b},")
+print(f"an Nth term of {n},")
+print(f"and a distance of {d},")
+print(f"")
